d1fa20dd89a0e1017676b06d1696b73d542f0196
David Markel <davidgmarkel@gmail.com>
2018-03-29 19:06:33 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,14 +3,16 @@ attr_accessor :a, :b, :c, :sides
 
 
   def initialize(a, b, c)
-  
-    self.sides = [a, b, c]
+    @a = a
+    @b = b
+    @c = c
+    @sides = [@a, @b, @c]
   end
 
   def kind
     if !@sides.detect {|side| side == 0}
       if @a + @b > @c && @b + @c > @a && @a + @c > @b
-        if @a == @b && @a == @c
+        if @sides.select {|side| side == side[0]}.count == @sides
         :equilateral
         elsif @a == @b || @a == @c || @b == @c
         :isosceles
